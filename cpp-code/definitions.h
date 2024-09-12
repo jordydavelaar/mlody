a654118481e43c716e094fec20609663797f53e1
Jordy Davelaar <jdavelaar@rustyamd1.cm.cluster>
2024-09-12 11:54:59 -0400

    working version

--- cpp-code/definitions.h
+++ cpp-code/definitions.h
@@ -1,5 +1,24 @@
+
 typedef struct GRMHD {
     double n_e;
     double B;
     double theta_e;
 } GRMHD;
+
+#define sign(x) (((x) < 0) ? -1 : ((x) > 0))
+
+// CONSTANTS
+////////////
+
+// PHYSICAL CONSTANTS
+#define ELECTRON_CHARGE (4.80320425e-10)
+#define ELECTRON_MASS (9.1093829e-28)
+#define PROTON_MASS (1.6726219e-24)
+#define BOLTZMANN_CONSTANT (1.3806488e-16)
+#define SPEED_OF_LIGHT (2.99792458e10)
+#define PLANCK_CONSTANT (6.62606885e-27)
+#define MPCL2 (0.0015033)
+#define GGRAV (6.674e-8)
+#define MSUN (1.989e33)
+#define KPCTOCM (3.086e21)
+#define MPoME (PROTON_MASS / ELECTRON_MASS)
--- cpp-code/main.c
+++ cpp-code/main.c
@@ -22,7 +22,7 @@ int main() {
     struct GRMHD modvar;
     double pitch_angle;
     double frequency;
-    double jI, aI, jQ, aQ,rQ, jU, aU, rU, jV, aV, rV;
+    double jI, aI, jQ, aQ,rQ, jV, aV, rV;
 
     init_ml_model();
 
@@ -33,33 +33,27 @@ int main() {
     pitch_angle = 0.5 * M_PI;
 
     fprintf(stdout, "Input Variables\n");
-    fprintf(stdout, "B = %f\n", modvar.B);
-    fprintf(stdout, "n_e = %f\n", modvar.n_e);
-    fprintf(stdout, "theta_e = %f\n", modvar.theta_e);
-    fprintf(stdout, "frequency = %f\n", frequency);
-    fprintf(stdout, "pitch_angle = %f\n", pitch_angle);
+    fprintf(stdout, "B = %lf\n", modvar.B);
+    fprintf(stdout, "n_e = %lf\n", modvar.n_e);
+    fprintf(stdout, "theta_e = %lf\n", modvar.theta_e);
+    fprintf(stdout, "frequency = %lf\n", frequency);
+    fprintf(stdout, "pitch_angle = %lf\n", pitch_angle);
 
-
-    mlody(frequency, modvar, pitch_angle, &jI, &aI, &jQ, &aQ, &rQ, &jU, &aU, &rU, &jV, &aV, &rV);
+    mlody(frequency, modvar, pitch_angle, &jI, &aI, &jQ, &aQ, &rQ, &jV, &aV, &rV);
 
     fprintf(stdout, "Stokes I\n");
-    fprintf(stdout, "jI = %f\n", jI);
-    fprintf(stdout, "aI = %f\n", aI);
+    fprintf(stdout, "jI = %e\n", jI);
+    fprintf(stdout, "aI = %e\n", aI);
 
     fprintf(stdout, "Stokes Q\n");
-    fprintf(stdout, "jQ = %f\n", jQ);
-    fprintf(stdout, "aQ = %f\n", aQ);
-    fprintf(stdout, "rQ = %f\n", rQ);
-
-    fprintf(stdout, "Stokes U\n");
-    fprintf(stdout, "jU = %f\n", jU);
-    fprintf(stdout, "aU = %f\n", aU);
-    fprintf(stdout, "rU = %f\n", rU);
+    fprintf(stdout, "jQ = %e\n", jQ);
+    fprintf(stdout, "aQ = %e\n", aQ);
+    fprintf(stdout, "rQ = %e\n", rQ);
 
     fprintf(stdout, "Stokes V\n");
-    fprintf(stdout, "jV = %f\n", jV);
-    fprintf(stdout, "aV = %f\n", aV);
-    fprintf(stdout, "rV = %f\n", rV);
+    fprintf(stdout, "jV = %e\n", jV);
+    fprintf(stdout, "aV = %e\n", aV);
+    fprintf(stdout, "rV = %e\n", rV);
         
     return 0;
 }
